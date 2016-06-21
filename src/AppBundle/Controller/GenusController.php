1101ef739dd871bcd3b72f295bf944572beff65c
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-06-21 23:03:37 +0200

    Do caching, parameters and yaml route

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -22,7 +22,7 @@ class GenusController extends Controller
         if ($cache->contains($key)) {
             $funFact = $cache->fetch($key);
         } else {
-            sleep(2);
+            sleep(1);
             $funFact = $this->get('markdown.parser')
                 ->transform($funFact);
             $cache->save($key, $funFact);
